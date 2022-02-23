5cf0271033cbaad1a6b56b6c7c2e8c886ae06e24
Matt Capelli <matt@simpledatatools.com>
2022-02-23 13:58:33 +0300

    Config admin panel for initial models

--- documents/admin.py
+++ documents/admin.py
@@ -1,18 +1,56 @@
 from django.contrib import admin
 
+from core.utils import randomstr
+
 from .models import *
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'category_id', 'workspace', 'status']
+    list_display = ['name', 'category_id', 'workspace', 'status']
+    fields = ['workspace', 'name', 'status', 'category_id', 'created_user', 'created_at', 'last_updated']
+    readonly_fields = ['category_id', 'created_user', 'created_at', 'last_updated']
+    search_fields = ('name', 'category_id')
+    list_per_page = 50
+
+    # Adds the public id and created user before saving
+    def save_model(self, request, obj, form, change):
+        if obj.category_id == None or obj.category_id == '':
+            obj.category_id = randomstr()
+        if obj.created_user == None:
+            obj.created_user = request.user
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Category, CategoryAdmin)
 
 class FieldAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'type', 'field_id', 'category', 'status']
+    list_display = ['name', 'type', 'category', 'field_id', 'status']
+    fields = ['category', 'name', 'type', 'status', 'field_id', 'created_user', 'created_at', 'last_updated']
+    readonly_fields = ['field_id', 'created_user', 'created_at', 'last_updated']
+    search_fields = ('name', 'field_id')
+    list_per_page = 50
+
+    # Adds the public id and created user before saving
+    def save_model(self, request, obj, form, change):
+        if obj.field_id == None or obj.field_id == '':
+            obj.field_id = randomstr()
+        if obj.created_user == None:
+            obj.created_user = request.user
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Field, FieldAdmin)
 
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'document_id', 'category', 'status']
+    list_display = ['document_id', 'category', 'status']
+    fields = ['category', 'status', 'document_id', 'created_user', 'created_at', 'last_updated']
+    readonly_fields = ['document_id', 'created_user', 'created_at', 'last_updated']
+    search_fields = ('document_id',)
+    list_per_page = 50
+
+    # Adds the public id and created user before saving
+    def save_model(self, request, obj, form, change):
+        if obj.document_id == None or obj.document_id == '':
+            obj.document_id = randomstr()
+        if obj.created_user == None:
+            obj.created_user = request.user
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Document, DocumentAdmin)
--- workspaces/admin.py
+++ workspaces/admin.py
@@ -1,21 +1,58 @@
 from django.contrib import admin
 
+from core.utils import randomstr
+
 from .models import *
 
 class WorkspaceAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'workspace_id', 'status']
-    fields = ['name', 'status']
+    list_display = ['name', 'workspace_id', 'status']
+    fields = ['name', 'status', 'workspace_id', 'created_user', 'created_at', 'last_updated']
+    readonly_fields = ['workspace_id', 'created_user', 'created_at', 'last_updated']
+    search_fields = ('name', 'workspace_id')
+    list_per_page = 50
+
+    # Adds the public id and created user before saving
+    def save_model(self, request, obj, form, change):
+        if obj.workspace_id == None or obj.workspace_id == '':
+            obj.workspace_id = randomstr()
+        if obj.created_user == None:
+            obj.created_user = request.user
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Workspace, WorkspaceAdmin)
 
+
 class PageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'label', 'page_id', 'page_slug', 'workspace', 'status']
-    fields = ['workspace', 'label', 'page_slug', 'status']
+    list_display = ['label', 'page_slug', 'workspace', 'page_id', 'status']
+    fields = ['workspace', 'label', 'page_slug', 'status', 'page_id', 'created_user', 'created_at', 'last_updated']
+    readonly_fields = ['page_id', 'created_user', 'created_at', 'last_updated']
+    search_fields = ('label', 'page_id', 'page_slug')
+    list_per_page = 50
+
+    # Adds the public id and created user before saving
+    def save_model(self, request, obj, form, change):
+        if obj.page_id == None or obj.page_id == '':
+            obj.page_id = randomstr()
+        if obj.created_user == None:
+            obj.created_user = request.user
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Page, PageAdmin)
 
+
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['id', 'label', 'tag_id', 'workspace', 'status']
-    fields = ['workspace', 'label', 'status']
+    list_display = ['label', 'workspace', 'tag_id', 'status']
+    fields = ['workspace', 'label', 'status', 'tag_id', 'created_user', 'created_at', 'last_updated']
+    readonly_fields = ['tag_id', 'created_user', 'created_at', 'last_updated']
+    search_fields = ('label', 'tag_id')
+    list_per_page = 50
+
+    # Adds the public id and created user before saving
+    def save_model(self, request, obj, form, change):
+        if obj.tag_id == None or obj.tag_id == '':
+            obj.tag_id = randomstr()
+        if obj.created_user == None:
+            obj.created_user = request.user
+        super().save_model(request, obj, form, change)
 
 admin.site.register(Tag, TagAdmin)
